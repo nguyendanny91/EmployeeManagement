7892c64a36a1a11b2d3707228c7e8094774c5183
Chuck <chuck.ausburn1@gmail.com>
2018-10-04 21:09:03 -0700

    Chuck's changes

---
+++ calcs.js
@@ -0,0 +1,9 @@
+//calculate Months Worked(integers) for each employee
+//(Current Date - Start Date)/30 (integers)
+
+
+//calucalate Total Billed amount for each employee
+//Monthly Rates * Months Worked(integers)
+
+
+//Push employee data () to the Firebase DB
